b9ea09349a9bf19bd3f7fdeda4d398edcd623778
unknown <trisna.tkjtamsisjetis14@gmail.com>
2022-04-29 09:47:43 -0700

    Changes to be committed: 	modified:   index.html 	modified:   js/plunge.js 	modified:   main.js

--- js/plunge.js
+++ js/plunge.js
@@ -1,7 +1,7 @@
 
 class Moder {
 
-    // request to ajax
+    // request to App
     connector = (HTML_Request,URL,request) => {
         
         import('./packages/App.js')
@@ -25,6 +25,31 @@ class Pen extends Moder {
         this.connector(HTML_Request,URL,(response) => request(response));
     }
 
+    typists_by = function (deliv,request) {
+
+        const HTML_Request = deliv.HTML_Request;
+
+        const button = document.getElementById(deliv.button_id);
+        button.onclick = () => {
+
+            let value = document.getElementById(deliv.input_id).value;
+            const URL = deliv.URL+value;
+
+            this.connector(HTML_Request,URL,(response) => request(response));
+        }
+    }
+
 }
 
+const compress = new Pen;
+
+var data = {
+    'URL'   : 'http://www.omdbapi.com?apikey=a97702a5&s=',
+    'HTML_Request' : 'GET',
+    'button_id' : 'tombol',
+    'input_id'  : 'cari'
+}
+console.log('1');
+compress.typists_by(data,(response) => console.log(response));
+console.log('2');
 
